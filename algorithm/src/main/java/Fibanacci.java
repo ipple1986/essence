44b96ca1aaacc1a364763eeefcf525ed5a0e103d
jim <ipple1986@gmail.com>
2018-11-18 17:07:12 +0800

    update

--- algorithm/src/main/java/Fibanacci.java
+++ algorithm/src/main/java/Fibanacci.java
@@ -25,16 +25,24 @@ public class Fibanacci {
     //公式大法 O(1)
     public static int foo4(int n){
         //S^2 - S - 1 = 0
-        //(x+a)(x+b)=0
-        //ab=-1
-        //a+b=-1
-        //a(1+a)=1
+        //a*x^2+b*x+c=0,x1= (-b + sqrt(b^2-4ac)/2a,x2= (-b - sqrt(b^2-4ac)/2a
+        //S1= (1+sqrt(5))/2, s2=(1-sqrt(5))/2
+        //代入 An = C1*(S1)^n + C2*(S2)^n,A1=1 A2=2
+        //得到 C1,C2
+        //C1 = (S2-2)/(S1S2-S1^2) = (3 + sqrt(5))/ (5 + sqrt(5))
+        //C2 = (S1-2)/(S1S2-S2^2) = (3 - sqrt(5))/ (5 + sqrt(5))
+        //最後 An = ((3 + sqrt(5))/ (5 + sqrt(5)))*((1+sqrt(5))/2)^n - ((3 - sqrt(5))/ (5 + sqrt(5)))*((1-sqrt(5))/2)^n
+        double c = Math.sqrt(5.0);
+        double c1 = (3 + c)/ (5 + c);
+        double c2 = (3 - c)/ (5 + c);
 
-        return 0;
+        return (int)Math.round((c1)*Math.pow((1+c)/2,n) - (c2)*Math.pow((1-c)/2,n));
+
     }
     public static void main(String args[]){
-        System.out.println(foo1(2));
-        System.out.println(foo2(2));
-        System.out.println(foo3(1,2,3,2));
+        System.out.println(foo1(13));
+        System.out.println(foo2(13));
+        System.out.println(foo3(1,2,3,13));
+        System.out.println(foo4(13));
     }
 }
