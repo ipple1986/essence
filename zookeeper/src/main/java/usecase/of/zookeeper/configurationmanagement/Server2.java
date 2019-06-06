992cd3332f4adb47cd632db4c3670a379fd5d7bb
Jim <ipple1986@gmail.com>
2019-06-06 14:50:29 +0800

    add config manager demos

---
+++ zookeeper/src/main/java/usecase/of/zookeeper/configurationmanagement/Server2.java
@@ -0,0 +1,40 @@
+package usecase.of.zookeeper.configurationmanagement;
+
+import org.apache.zookeeper.*;
+import usecase.of.zookeeper.barrier.AbstractZooKeeperHelper;
+
+public class Server2 extends AbstractZooKeeperHelper {
+
+
+    private String redisHost = "";
+    private String redisHostZnode = "/redisHost";
+    public Server2(){
+        try {
+            if(zk.exists(redisHostZnode,false)==null){
+                zk.create(redisHostZnode,"127.0.0.1:6379".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+            redisHost = new String(zk.getData(redisHostZnode,myWatcher,null));
+            System.out.println();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    private Watcher myWatcher = new Watcher() {
+        @Override
+        public void process(WatchedEvent watchedEvent) {
+            if(watchedEvent.getType() == Event.EventType.NodeDataChanged){
+                try {
+                    redisHost = new String(zk.getData(redisHostZnode,myWatcher,null));
+                    System.out.println("Server2 recevice event:[resitHost="+redisHost+"]");
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+}
